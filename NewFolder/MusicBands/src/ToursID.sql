a18ec64b4cab2df753a0b86a3da00ed30a166622
Natali Trefilova <mionavlika@ymail.com>
2013-12-15 18:08:34 +0400

    Tours + Main-GUI

---
+++ NewFolder/MusicBands/src/ToursID.sql
@@ -0,0 +1,9 @@
+/**
+ *
+ * @author natali
+ * @name ToursID
+ */ 
+Select  t2.T_ID, t.G_ID, t2.SCHEDULE_OF_TOUR_ID, Name_of_town, StartDate, EndDate, Average_price
+From GROUPS t1
+ Inner Join TOURS t on t.G_ID = t1.G_ID
+ Inner Join SCHEDULE_OF_TOUR t2 on t2.T_ID = t.T_ID
---
+++ NewFolder/MusicBands/src/qCreateTour.sql
@@ -0,0 +1,8 @@
+/**
+ *
+ * @author natali
+ * @name qCreateTour
+ */ 
+Select * 
+From qSearchTour q1
+ Where :tID = q1.T_ID
